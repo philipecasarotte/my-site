2ac93a4a03cdd718bda22500bb42093d8fc019af
Philipe Casarotte <philipetauk@gmail.com>
2015-08-04 20:39:29 -0700

    replace ga code

--- index.php
+++ index.php
@@ -223,18 +223,19 @@
 		
 		<!-- Drummer & Coder, I may miss a comma, but never the beat. -->
 		 
-		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
-		 <script type="text/javascript" src="/min/f=javascripts/default.js"></script>
-		 
-		 <script type="text/javascript">
-			(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+		<script type="text/javascript" src="/min/f=javascripts/default.js"></script>
+			
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			ga("create", "UA-63270429-1", "auto");
-			ga("send", "pageview");
-			ga("require", "displayfeatures");
-		 </script>
+			ga('create', 'UA-63270429-2', 'auto');
+			ga('send', 'pageview');
+			ga('require', 'displayfeatures');
+			ga('set', '&uid', {{USER_ID}});
+		</script>
 	</body>
 </html>
