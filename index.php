c38d498e77ea75229819d7cbea8c0c07db2f2746
Philipe Casarotte <philipetauk@gmail.com>
2015-11-12 07:37:31 -0800

    change og image

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 		<meta name="google-site-verification" content="_Ao4wv72VkOYnYgvu27J2vGEUOhSnDTXPq56_x9LZiM"/>
-		<meta property="og:image" content="http://casarotte.com/images/philipe-casarotte-website.png"/>
+		<meta property="og:image" content="http://casarotte.com/images/philipe-casarotte-website.png?007"/>
 		
 		<link rel="apple-touch-icon" href="images/favicon.png"/>
 		<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico"/>
